cf4b3935097fc8553c29ee0ab39d33e6a3db9057
thamer-souelmi <thamer.souelmi@esprit.tn>
2024-02-27 03:08:01 +0100

    test 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplatBackComponent } from './BackOffice/all-templat-back/all-templat-back.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:"admin",
+  component : AllTemplatBackComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTemplatBackComponent } from './BackOffice/all-templat-back/all-templat-back.component';
+import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTemplatBackComponent,
+    FooterBackComponent,
+    NavbarBackComponent,
+    SidebarBackComponent
   ],
   imports: [
     BrowserModule,
