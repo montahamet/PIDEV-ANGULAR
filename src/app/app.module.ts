45d8daea6a1d43d06be39ab296ef94b69e2249e2
Montaha Metjaouel <montaha.metjaouel@esprit.tn>
2024-02-27 01:33:31 +0100

    git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTemplateFrontComponent } from '../assets/FrontOffice/all-template-front/all-template-front.component';
+import { FooterFrontComponent } from '../assets/FrontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from '../assets/FrontOffice/header-front/header-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTemplateFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent
   ],
   imports: [
     BrowserModule,
