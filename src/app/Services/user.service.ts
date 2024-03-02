552b2706376e3b92742b67400fc97771044e9ecd
user <haifabensalah81@gmail.com>
2024-03-02 16:45:00 +0100

    models trainingsession

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from '../Models/user';
+import { User } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
